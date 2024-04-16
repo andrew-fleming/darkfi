4dbdaf01799b01ddf72d2ca4b7e8cd7c5141a6e9
bookworm <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 23:33:47 +0000

    Generate mdbook

---
+++ dev/darkfid/proto/protocol_sync/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["BATCH"],"struct":["ForkSyncRequest","ForkSyncResponse","HeaderSyncRequest","HeaderSyncResponse","IsSyncedRequest","IsSyncedResponse","ProtocolSync","SyncRequest","SyncResponse","TipRequest","TipResponse"]};
